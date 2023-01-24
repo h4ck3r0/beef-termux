345fec6cade252ab55c3c604bc8ee06ca3ac8fa8
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2023-01-24 20:26:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ pkg upgrade -y -o Dpkg::Options::="--force-confnew"
 echo
 center "*** needs binutils installation..."
 
-pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+pkg install -y binutils python git autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
 pkg install -y curl wget libyaml bison espeak nodejs 
 
 python3 -m pip install --upgrade pip
@@ -38,4 +38,4 @@ cd $PREFIX/opt
 git clone https://github.com/beefproject/beef --depth=1
 termux-open-url https://termux.xyz/how-to-install-beef-in-termux/
 
-echo
+echo ""
