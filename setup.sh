5d7f3d2124df25fe192a71fa41b7903411685f7e
Raj Aryan <46929618+h4ck3r0@users.noreply.github.com>
2026-05-26 00:47:55 +0530

    Refactor setup script for improved readability

--- setup.sh
+++ setup.sh
@@ -1,49 +1,59 @@
-clear
+#!/bin/bash
 
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-echo -e "${RED} ███████ ▓█████▄▄▄█████▓ █    ██  ██▓███  " 
+echo -e "${RED} ███████ ▓█████▄▄▄█████▓ █   ██  ██▓███  " 
 echo -e "${RED}▒██    ▒ ▓█   ▀▓  ██▒ ▓▒ ██  ▓██▒▓██░  ██▒" 
 echo -e "${RED}░ ▓██▄   ▒███  ▒ ▓██░ ▒░▓██  ▒██░▓██░ ██▓▒" 
 echo -e "${RED}  ▒   ██▒▒▓█  ▄░ ▓██▓ ░ ▓▓█  ░██░▒██▄█▓▒ ▒" 
 echo -e "${RED}▒██████▒▒░▒████▒ ▒██▒ ░ ▒▒█████▓ ▒██▒ ░  ░" 
 echo -e "${RED}▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░   ░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░" 
 echo -e "${RED}░ ░▒  ░ ░ ░ ░  ░   ░    ░░▒░ ░ ░ ░▒ ░     " 
-echo -e "${RED}░  ░  ░     ░    ░       ░░░ ░ ░ ░░       " 
-echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   " 
+echo -e "${RED}░  ░  ░     ░    ░      ░░░ ░ ░ ░░        " 
+echo -e "${RED}     ░     ░  ░         ░            ${ENDCOLOR}   " 
                                           
-echo  ""                                          
-echo -e "\e[1;34m[*] \e[32mInstalling Requirements\e[0m";   
+echo ""                                         
+echo -e "\e[1;34m[*] \e[32mInstalling Requirements\e[0m"   
 
+# Clean old ruby gems to prevent conflicts
 apt purge ruby -y
 rm -fr $PREFIX/lib/ruby/gems
 
+pkg update -y
 pkg upgrade -y -o Dpkg::Options::="--force-confnew"
 
-# needs binutils
-echo
-center "*** needs binutils installation..."
+echo "*** Installing binutils and dependencies..."
 
-pkg install -y binutils python git autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
-pkg install -y curl wget libyaml bison espeak nodejs 
+pkg install -y binutils python git autoconf bison clang coreutils curl findutils \
+apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc \
+libtool libxml2 libxslt ncurses make ncurses-utils wget unzip zip tar termux-tools \
+termux-elf-cleaner pkg-config ruby libyaml espeak nodejs -o Dpkg::Options::="--force-confnew"
 
+# Upgrade pip and install requests
 python3 -m pip install --upgrade pip
 python3 -m pip install requests
 
-echo
-center "*** Downloading..."
-cd $PREFIX/opt
+echo "*** Downloading BeEF..."
+cd $HOME
+
+# Remove existing directory if reinstalling
+if [ -d "beef" ]; then
+    rm -rf beef
+fi
+
 git clone https://github.com/beefproject/beef --depth=1
-termux-open-url https://termux.xyz/how-to-install-beef-in-termux/
-cd $HOME/beef-termux/
+cd $HOME/beef
+
+# Install ruby dependencies
 gem install bundler
-gem install nokogiri -- --use-system-libraries
-sed -i 's/nio4r (2.5.8)/nio4r (2.5.9)/' Gemfile.lock
-sed -i '268,274d' install
-sed -i '26,181d' install
-sed -i 's/sudo//' install
+NOKOGIRI_USE_SYSTEM_LIBRARIES=1 gem install nokogiri -- --use-system-libraries
+
+# Patching the install script to remove sudo requirements for Termux
+sed -i 's/sudo //g' install
+
+# Run the installer
 bash install
 
-echo ""
+echo -e "\e[1;34m[*] \e[32mSetup Complete.\e[0m"
